8312c45edcf5fac217937833d6235e1eaa915988
OBIORA <obiorakingsley5050@gmail.com>
2023-11-30 23:57:12 +0100

    done

--- frontend/src/components/SearchBox.tsx
+++ frontend/src/components/SearchBox.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
-import { Button, Form, FormControl, InputGroup } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { Button, Form, FormControl, InputGroup } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchBox() {
-  const navigate = useNavigate()
-  const [query, setQuery] = useState('')
+  const navigate = useNavigate();
+  const [query, setQuery] = useState("");
 
   const submitHandler = (e: React.SyntheticEvent) => {
-    e.preventDefault()
-    navigate(query ? `/search?query=${query}` : '/search')
-  }
+    e.preventDefault();
+    navigate(query ? `/search?query=${query}` : "/search");
+  };
   return (
     <Form className="flex-grow-1 d-flex me-auto" onSubmit={submitHandler}>
       <InputGroup>
@@ -17,7 +17,7 @@ export default function SearchBox() {
           type="text"
           name="q"
           id="q"
-          placeholder="Search Amazona"
+          placeholder="Search ShopEazy"
           aria-label="Search Amazona"
           aria-describedby="button-search"
           onChange={(e) => setQuery(e.target.value)}
@@ -27,5 +27,5 @@ export default function SearchBox() {
         </Button>
       </InputGroup>
     </Form>
-  )
+  );
 }
